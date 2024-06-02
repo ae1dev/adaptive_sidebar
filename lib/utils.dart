de54b4290875bc4f78b39c2c4b581a1849e20d41
Elias <elias@deuss.dev>
2024-06-02 20:57:21 +0200

    Create utils.dart

---
+++ lib/utils.dart
@@ -0,0 +1,8 @@
+part of adaptive_sidebar;
+
+// double getAdaptiveSidebarSize(BuildContext context, double maxLargeSidebarSize) {
+//   if (MediaQuery.of(context).size.width <= 700) {
+//     return 0;
+//   }
+//   return maxLargeSidebarSize; 
+// }
