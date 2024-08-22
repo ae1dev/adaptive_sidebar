25a5d80a46d52add26dfa19fbacbb20490130f78
Elias <elias@deuss.dev>
2024-08-22 11:45:40 +0200

    feat: tooltip on icon only mode

--- lib/widgets/as_destination.dart
+++ lib/widgets/as_destination.dart
@@ -44,55 +44,60 @@ class _ASDestinationState extends State<ASDestination> {
             hovering = false;
           }),
           cursor: SystemMouseCursors.click,
-          child: Container(
-            padding: EdgeInsets.only(
-                right: widget.iconsOnly ? 5 : 8, left: 5, bottom: 4, top: 4),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                //IconData Icon
-                if (widget.destination.icon != null)
-                  Container(
-                    margin: EdgeInsets.only(
-                        right: widget.iconsOnly ? 0 : 8, bottom: 2),
-                    child: Icon(
-                      widget.destination.icon,
-                      fill: widget.selected ? 1 : 0,
-                      color: getTextColor(context),
-                    ),
-                  ),
-                //Widget Icon
-                if (widget.destination.icon == null &&
-                    widget.destination.iconBuilder != null)
-                  Container(
-                    margin: EdgeInsets.only(
-                        right: widget.iconsOnly ? 0 : 8, bottom: 2),
-                    child: widget.destination.iconBuilder!(
-                      context,
-                      getTextColor(context),
-                    ),
-                  ),
-                if (!widget.iconsOnly)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 2),
-                      child: AutoSizeText(
-                        widget.destination.label,
-                        style:
-                            Theme.of(context).textTheme.displayLarge!.copyWith(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  color: getTextColor(context),
-                                ),
-                        minFontSize: 16,
-                        maxLines: 1,
-                        softWrap: false,
-                        overflow: TextOverflow.fade,
+          child: Tooltip(
+            message: widget.iconsOnly ? widget.destination.label : null,
+            child: Container(
+              padding: EdgeInsets.only(
+                  right: widget.iconsOnly ? 5 : 8, left: 5, bottom: 4, top: 4),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  //IconData Icon
+                  if (widget.destination.icon != null)
+                    Container(
+                      margin: EdgeInsets.only(
+                          right: widget.iconsOnly ? 0 : 8, bottom: 2),
+                      child: Icon(
+                        widget.destination.icon,
+                        fill: widget.selected ? 1 : 0,
+                        color: getTextColor(context),
                       ),
                     ),
-                  ),
-              ],
+                  //Widget Icon
+                  if (widget.destination.icon == null &&
+                      widget.destination.iconBuilder != null)
+                    Container(
+                      margin: EdgeInsets.only(
+                          right: widget.iconsOnly ? 0 : 8, bottom: 2),
+                      child: widget.destination.iconBuilder!(
+                        context,
+                        getTextColor(context),
+                      ),
+                    ),
+                  if (!widget.iconsOnly)
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2),
+                        child: AutoSizeText(
+                          widget.destination.label,
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayLarge!
+                              .copyWith(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                color: getTextColor(context),
+                              ),
+                          minFontSize: 16,
+                          maxLines: 1,
+                          softWrap: false,
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
             ),
           ),
         ),
