f789a860400722336ded6b9ba97e79fa7b623dcb
Elias <elias@deuss.dev>
2024-11-28 22:27:49 +0100

    chore: remove unused package auto_size_text

--- lib/adaptive_sidebar.dart
+++ lib/adaptive_sidebar.dart
@@ -1,6 +1,5 @@
 library adaptive_sidebar;
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
